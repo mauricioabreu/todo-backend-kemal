4ef4d78cb8905e25f807a9deac6e65b32233abe8
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-06-23 00:06:57 -0300

    Decouple todo entries from a list
    It is not necessary for the problem to have more than one todo list.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,3 @@
 require "spec"
-require "../src/todo/*"
+require "spec-kemal"
+require "../src/todo"
--- src/todo/entities.cr
+++ src/todo/entities.cr
@@ -1,54 +1,22 @@
+require "json"
+
 module Todo
-  class TodoList
-    def initialize(@_id : String)
-      @todo_list = Array(TodoItem).new
-    end
-
-    def _id
-      @_id
-    end
-
-    def todo_list
-      @todo_list
-    end
-
-    def add_item(item : TodoItem)
-      @todo_list.push(item)
-    end
-
-    def remove_item(item_id : String)
-      @todo_list = @todo_list.reject { |i| i._id == item_id }
-    end
-
-    def clear
-      @todo_list = Array(TodoItem).new
-    end
-
-    def get_item(item_id : String)
-      @todo_list.find do |item|
-        item._id == item_id
-      end
-    end
-  end
-
   class TodoItem
-    def initialize(@name : String, @_id : String, @done = false)
+    JSON.mapping({
+      _id: {type: String, nillable: true},
+      title: String,
+      completed: {type: Bool, nillable: true, default: false}
+    })
+
+    def initialize(@_id : String, @title : String, @completed : Bool)
     end
 
     def _id
       @_id
-    end
-
-    def name
-      @name
     end
 
     def is_done
-      @done
-    end
-
-    def mark_as_done
-      @done = true
+      @completed
     end
   end
 end
