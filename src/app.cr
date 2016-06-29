be8bc74a1bbd8aca2c91248d35f2eead6bb5d858
Mauricio Antunes <mauricio.antunes@azion.com>
2016-06-29 13:31:20 -0300

    Add repr

--- src/app.cr
+++ src/app.cr
@@ -6,6 +6,16 @@ TODOS_URI = "/todos"
 
 Handler = Todo::TodoHandler.new Todo::TodoRepository.new
 
+def base_url(env)
+	env.request.scheme + "://" + env.request.host
+end
+
+def repr(todo)
+	{"uid"   => todo._id,
+	 "title" => todo.title,
+	 "order" => todo.order,
+	 "url"	 => "#{TODOS_URI}/#{todo._id}"}
+end
 
 get "/" do |env|
   env.redirect TODOS_URI
@@ -13,14 +23,20 @@ end
 
 get "/todos" do
   todos = Handler.list_todos
-  todos.to_json
+	todos.map{ |todo| repr(todo)}.to_json
+end
+
+get "/todos/:id" do |env|
+	id = env.params.url["id"] as String
+	todo = Handler.get_todo id
+	repr(todo).to_json
 end
 
 post "/todos" do |env|
   title = env.params.json["title"] as String
   todo = Handler.add_todo_item title
   env.response.status_code = 201
-  todo.to_json
+	repr(todo).to_json
 end
 
 delete "/todos" do |env|
