c2aaf5d804546c335113628d4fbb1c5c3fb74820
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-06-29 23:17:07 -0300

    Add headers

--- src/app.cr
+++ src/app.cr
@@ -1,42 +1,59 @@
 require "./todo"
 require "kemal"
 
-
+# Base resource path.
 TODOS_URI = "/todos"
 
 Handler = Todo::TodoHandler.new Todo::TodoRepository.new
 
 def base_url(env)
-	env.request.scheme + "://" + env.request.host
+  # Set base url like this because Kemal does not
+  # support URI parse like Sinatra does.
+  # See: https://github.com/sdogruyol/kemal/issues/148
+  "http://" + env.request.headers["host"]
 end
 
-def repr(todo)
-	{"uid"   => todo._id,
-	 "title" => todo.title,
-	 "order" => todo.order,
-	 "url"	 => "#{TODOS_URI}/#{todo._id}"}
+def todo_url(id)
+  "#{TODOS_URI}/#{id}"
 end
+
+def repr(todo, base_url)
+  {"uid"   => todo._id,
+    "title" => todo.title,
+    "order" => todo.order,
+    "url"   => base_url + todo_url(todo._id)
+  }
+end
+
+before_all "/todos" do |env|
+  # Support CORS and set responses to JSON as default.
+  headers env, {
+    "Access-Control-Allow-Origin" => "*",
+    "Content-Type" => "application/json"
+  }
+end
+
 
 get "/" do |env|
   env.redirect TODOS_URI
 end
 
-get "/todos" do
+get "/todos" do |env|
   todos = Handler.list_todos
-	todos.map{ |todo| repr(todo)}.to_json
+  todos.map { |todo| repr(todo, base_url(env)) }.to_json
 end
 
 get "/todos/:id" do |env|
-	id = env.params.url["id"] as String
-	todo = Handler.get_todo id
-	repr(todo).to_json
+  id = env.params.url["id"].as(String)
+  todo = Handler.get_todo id
+  repr(todo, base_url(env)).to_json
 end
 
 post "/todos" do |env|
-  title = env.params.json["title"] as String
+  title = env.params.json["title"].as(String)
   todo = Handler.add_todo_item title
   env.response.status_code = 201
-	repr(todo).to_json
+  repr(todo, base_url(env)).to_json
 end
 
 delete "/todos" do |env|
@@ -45,7 +62,7 @@ delete "/todos" do |env|
 end
 
 delete "/todos/:id" do |env|
-  id = env.params.url["id"] as String
+  id = env.params.url["id"].as(String)
   Handler.remove_todo_item id
   env.response.status_code = 204
 end
