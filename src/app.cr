618f9a8c6c074f4017b667774faa1b33acf74b8d
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-06-30 17:49:00 -0300

    Add views

---
+++ src/app.cr
@@ -0,0 +1,80 @@
+require "./todo"
+require "kemal"
+
+# Base resource path.
+TODOS_URI = "/todos"
+
+Handler = Todo::TodoHandler.new Todo::TodoRepository.new
+
+def base_url(env)
+  # Set base url like this because Kemal does not
+  # support URI parse like Sinatra does.
+  # See: https://github.com/sdogruyol/kemal/issues/148
+  "http://" + env.request.headers["host"]
+end
+
+def todo_url(id)
+  "#{TODOS_URI}/#{id}"
+end
+
+def repr(todo, base_url)
+  {"uid"   => todo._id,
+    "title" => todo.title,
+    "order" => todo.order,
+    "url"   => base_url + todo_url(todo._id)
+  }
+end
+
+before_all "/todos" do |env|
+  # Support CORS and set responses to JSON as default.
+  headers env, {
+    "Access-Control-Allow-Origin" => "*",
+    "Content-Type" => "application/json"
+  }
+end
+
+
+get "/" do |env|
+  env.redirect TODOS_URI
+end
+
+get "/todos" do |env|
+  todos = Handler.list_todos
+  todos.map { |todo| repr(todo, base_url(env)) }.to_json
+end
+
+get "/todos/:id" do |env|
+  id = env.params.url["id"].as(String)
+  todo = Handler.get_todo id
+  repr(todo, base_url(env)).to_json
+end
+
+post "/todos" do |env|
+  title = env.params.json["title"].as(String)
+  todo = Handler.add_todo_item title
+  env.response.status_code = 201
+  repr(todo, base_url(env)).to_json
+end
+
+patch "/todos/:id" do |env|
+  id = env.params.url["id"].as(String)
+  todo = Handler.get_todo id
+  title = env.params.json.fetch("title", todo.title) as String
+  completed = env.params.json.fetch("completed", todo.completed) as Bool
+  order = env.params.json.fetch("order", todo.order) as (Int32 | Int64)
+  todo = Handler.update_todo_item id, item_title: title, completed: completed, order: order
+  repr(todo, base_url(env)).to_json
+end
+
+delete "/todos" do |env|
+  Handler.clear_todo
+  env.response.status_code = 204
+end
+
+delete "/todos/:id" do |env|
+  id = env.params.url["id"].as(String)
+  Handler.remove_todo_item id
+  env.response.status_code = 204
+end
+
+Kemal.run
